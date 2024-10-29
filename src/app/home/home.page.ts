37861f4063406766cdab4059b9c542b588b50a99
RaphaelNunes10 <raphaelnunes10@outlook.com>
2024-10-29 18:13:37 -0300

    feat: remove message example

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { RefresherCustomEvent } from '@ionic/angular';
-import { MessageComponent } from '../message/message.component';
-
-import { DataService, Message } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -10,16 +7,4 @@ import { DataService, Message } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  private data = inject(DataService);
-  constructor() {}
-
-  refresh(ev: any) {
-    setTimeout(() => {
-      (ev as RefresherCustomEvent).detail.complete();
-    }, 3000);
-  }
-
-  getMessages(): Message[] {
-    return this.data.getMessages();
-  }
 }
