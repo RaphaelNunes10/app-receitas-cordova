b8b35a3007d247017d37fd95631bb94ab692481d
RaphaelNunes10 <raphaelnunes10@outlook.com>
2024-10-31 18:31:06 -0300

    feat: improve recita interface

--- src/models/receita.interface.ts
+++ src/models/receita.interface.ts
@@ -1,11 +1,24 @@
+export type PeriodoPreparo = `Segundos${'s' | ''}` | `Minuto${'s' | ''}` | `Hora${'s' | ''}`
+export type MedidaPorcao = `Unidade${'s' | ''}` | `Pedaço${'s' | ''}` | `Fatia${'s' | ''}` | 'g' | 'kg' | 'L' | `Pessoa${'s' | ''}`
+export type MedidaIngrediente = 'Colher de Café' | 'Colher de Chá' | 'Colher de Sobremesa' | 'Colher de Sopa' | 'mg' | 'g' | 'Kg' | 'ml' | 'L'
+
+type UrlImagem = `http${'s' | ''}://${string}`
+type ImagemBase64 = `data:image/${string};base64,${string}`
+type TempoPreparo = `${number} ${PeriodoPreparo}`
+type Porcao = `${number} ${MedidaPorcao}`
+type Ingrediente = `${number} ${MedidaIngrediente} de ${string}`
+type Utensilio = `${number}x ${string}`
+
 export interface Receita {
   id: string,
   titulo: string,
-  imagem: `http${'s' | ''}://${string}` | `data:image/${string};base64,${string}`,
-  info: string,
+  imagens: string[],
+  descricao: string,
   preparo: string,
-  tempoPreparo: `${number} ${`Segundos${'s' | ''}` | `Minuto${'s' | ''}` | `Hora${'s' | ''}`}`,
-  porcao: `${number} ${`Unidade${'s' | ''}` | `Pedaço${'s' | ''}` | `Fatia${'s' | ''}` | 'g' | 'kg' | 'L' | `Pessoa${'s' | ''}`}`,
-  utensilhos: `${number}x ${string}`[],
-  dataCriacao: Date
+  tempoPreparo: TempoPreparo | '',
+  porcao: Porcao | '',
+  ingredientes: Ingrediente[],
+  utensilios: Utensilio[],
+  notasAdicionais: string,
+  dataCriacao: Date | undefined
 }
