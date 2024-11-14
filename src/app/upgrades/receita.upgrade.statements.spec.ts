2e0cbc0c4324f809b0563e1e58af7657b659c55c
RaphaelNunes10 <raphaelnunes10@outlook.com>
2024-11-14 16:58:35 -0300

    feat: simplify and improve receita upgrade statements

---
+++ src/app/upgrades/receita.upgrade.statements.spec.ts
@@ -0,0 +1,7 @@
+import { ReceitaUpgradeStatements } from './receita.upgrade.statements';
+
+describe('ReceitaUpgradeStatements', () => {
+  it('should create an instance', () => {
+    expect(new ReceitaUpgradeStatements()).toBeTruthy();
+  });
+});
