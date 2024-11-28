1fb3970d8104d6f6f650242fad31a20577600efe
RaphaelNunes10 <raphaelnunes10@outlook.com>
2024-11-28 16:27:56 -0300

    style: prettify receita service

--- src/app/services/receita.service.ts
+++ src/app/services/receita.service.ts
@@ -5,29 +5,27 @@ import { Preferences } from '@capacitor/preferences';
 import { Receita } from '../models/receita';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReceitaService {
-    public receitas: Receita[];
+  public receitas: Receita[];
 
-    constructor() {
-        this.receitas = [];
-    }
+  constructor() {
+    this.receitas = [];
+  }
 
-    public async createReceita(receita: Receita) {
-        this.receitas.push(receita);
+  public async createReceita(receita: Receita) {
+    this.receitas.push(receita);
 
-        console.log(this.receitas);
+    await Preferences.set({
+      key: 'receitas',
+      value: JSON.stringify(this.receitas),
+    });
+  }
 
-        await Preferences.set({
-            key: 'receitas',
-            value: JSON.stringify(this.receitas),
-        });
-    }
+  public async fetchReceita() {
+    const { value } = await Preferences.get({ key: 'receitas' });
 
-    public async fetchReceita() {
-        const { value } = await Preferences.get({ key: 'receitas' });
-
-        value ? this.receitas = JSON.parse(value) : this.receitas = [];
-    }
+    value ? (this.receitas = JSON.parse(value)) : (this.receitas = []);
+  }
 }
